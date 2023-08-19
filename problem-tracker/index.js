dd8e40cc88aefb6a5ee890858c359bc0d1848612
valor <61257192+taintedvalor@users.noreply.github.com>
2023-08-19 01:52:44 -0400

    Update index.js

--- problem-tracker/index.js
+++ problem-tracker/index.js
@@ -1,34 +1,60 @@
- 
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 
 app.use(express.static('public'));
+app.use(express.json());
 
-// Store the problems in an array (this is for demonstration purposes, you should use a database in a production environment)
+// Store the problems in an array (for demonstration purposes; use a database in production)
 const problems = [];
 
+// Function to generate a unique ID for a problem
+function generateProblemId() {
+    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+}
+
+// Route to retrieve all problems
 app.get('/problems', (req, res) => {
-  res.json(problems);
+    res.json(problems);
 });
 
-app.post('/problems', express.json(), (req, res) => {
-  const newProblem = req.body;
-  problems.push(newProblem);
-  res.sendStatus(201); // Created
+// Route to add a new problem
+app.post('/problems', (req, res) => {
+    const newProblem = req.body;
+    newProblem.id = generateProblemId();
+    problems.push(newProblem);
+    res.status(201).json(newProblem);
 });
 
+// Route to update a problem's status
+app.patch('/problems/:id', (req, res) => {
+    const idToUpdate = req.params.id;
+    const { status } = req.body;
+    
+    const problemIndex = problems.findIndex(problem => problem.id === idToUpdate);
+
+    if (problemIndex === -1) {
+        res.sendStatus(404); // Not Found
+    } else {
+        problems[problemIndex].status = status;
+        res.sendStatus(204); // No Content (successful update)
+    }
+});
+
+// Route to delete a problem
 app.delete('/problems/:id', (req, res) => {
-  const idToDelete = req.params.id;
-  const index = problems.findIndex(problem => problem.id === idToDelete);
-  if (index !== -1) {
-    problems.splice(index, 1);
-    res.sendStatus(204); // No Content (successful delete)
-  } else {
-    res.sendStatus(404); // Not Found
-  }
+    const idToDelete = req.params.id;
+    const problemIndex = problems.findIndex(problem => problem.id === idToDelete);
+
+    if (problemIndex === -1) {
+        res.sendStatus(404); // Not Found
+    } else {
+        problems.splice(problemIndex, 1);
+        res.sendStatus(204); // No Content (successful delete)
+    }
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
