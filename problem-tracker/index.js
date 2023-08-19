c7c163ccf61ac0e76ea38e6672235a71f1452a99
valor <61257192+taintedvalor@users.noreply.github.com>
2023-08-19 10:57:29 -0400

    Update index.js

--- problem-tracker/index.js
+++ problem-tracker/index.js
@@ -1,68 +1,69 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-// In-memory storage for problems (you can replace this with a database)
+// Parse JSON and URL-encoded body parameters
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Store the list of problems in memory (you might want to use a database in a real application)
 const problems = [];
 
-// API endpoint to add a new problem
-app.post('/api/problems', (req, res) => {
-    const { title, description, status } = req.body;
+// Define your routes
 
-    if (!title || !description || !status) {
-        return res.status(400).json({ error: 'Title, description, and status are required.' });
-    }
-
-    const newProblem = {
-        id: problems.length + 1,
-        title,
-        description,
-        status
-    };
-
-    problems.push(newProblem);
-
-    res.status(201).json(newProblem);
-});
-
-// API endpoint to get all problems
+// Route to get the list of problems
 app.get('/api/problems', (req, res) => {
-    res.json(problems);
+  res.json(problems);
 });
 
-// API endpoint to update the status of a problem by ID
-app.put('/api/problems/:id', (req, res) => {
-    const { id } = req.params;
-    const { status } = req.body;
+// Route to add a new problem
+app.post('/api/problems', (req, res) => {
+  const { title, description } = req.body;
+  if (!title || !description) {
+    return res.status(400).json({ error: 'Title and description are required.' });
+  }
 
-    const problem = problems.find(p => p.id === parseInt(id));
-
-    if (!problem) {
-        return res.status(404).json({ error: 'Problem not found.' });
-    }
-
-    problem.status = status;
-
-    res.json(problem);
+  const newProblem = {
+    title,
+    description,
+    status: 'unhandled',
+  };
+  problems.push(newProblem);
+  res.status(201).json(newProblem);
 });
 
-// API endpoint to delete a problem by ID
+// Route to update the status of a problem
+app.patch('/api/problems/:id', (req, res) => {
+  const { id } = req.params;
+  const { status } = req.body;
+
+  const problem = problems[id];
+
+  if (!problem) {
+    return res.status(404).json({ error: 'Problem not found.' });
+  }
+
+  problem.status = status;
+  res.json(problem);
+});
+
+// Route to delete a problem
 app.delete('/api/problems/:id', (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    const index = problems.findIndex(p => p.id === parseInt(id));
+  if (!problems[id]) {
+    return res.status(404).json({ error: 'Problem not found.' });
+  }
 
-    if (index === -1) {
-        return res.status(404).json({ error: 'Problem not found.' });
-    }
-
-    problems.splice(index, 1);
-
-    res.sendStatus(204);
+  problems.splice(id, 1);
+  res.sendStatus(204);
 });
 
+// Start the server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
